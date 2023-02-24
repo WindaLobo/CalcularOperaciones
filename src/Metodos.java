0e4a6f24b6a9e62a61a48b4246e69a3e3ccea10d
WindaLobo <wlobogarces@danielcastelao.org.com>
2023-02-24 09:58:59 +0100

    actualizacion

--- src/Metodos.java
+++ src/Metodos.java
@@ -16,6 +16,11 @@ public class Metodos {
      */
     private static final String Division = "/";
     /**
+     * Creo un atributo privado static final de Raiz = "%".
+     */
+    private static final String Raiz = "%";
+
+    /**
      * Creo un atributo privado static  de total en donde guardar los resultados .
      */
     private static Float total = null;
@@ -28,10 +33,10 @@ public class Metodos {
     public static void realizarOperacionBasicas() {
 
         try {
-            String operacion = LibreriaEntradSalida.entradaString("Que operacion quieres hacer " + "( \"+\" = Suma , \"-\" = Resta , \"*\" " + "= Multiplicacion , \"/\" = Division",LibreriaEntradSalida.CONSOLA);
+            String operacion = LibreriaEntradSalida.entradaString("Que operacion quieres hacer " + "( \"+\" = Suma , \"-\" = Resta , \"*\" " + "= Multiplicacion , \"/\" = Division ,  \"%\" = RaizCubico ", LibreriaEntradSalida.CONSOLA);
             validarOperacion(operacion);
-            float a = LibreriaEntradSalida.recibefloat("Introduce un numero",LibreriaEntradSalida.CONSOLA);
-            float b = LibreriaEntradSalida.recibefloat("Introduce un numero",LibreriaEntradSalida.CONSOLA);
+            float a = LibreriaEntradSalida.recibefloat("Introduce un numero", LibreriaEntradSalida.CONSOLA);
+            float b = LibreriaEntradSalida.recibefloat("Introduce un numero", LibreriaEntradSalida.CONSOLA);
             float resultado = operacionesBasicas(a, b, operacion);
             LibreriaEntradSalida.salida("El resulta es " + resultado, LibreriaEntradSalida.CONSOLA);
         } catch (Exception ex) {
@@ -43,7 +48,7 @@ public class Metodos {
     /**
      * Creo un método privado de operacionesBasicas que recibe tres parametros, float numero1, float numero2, String operacion
      *
-     * @param operacion tendra varias opciones, sumar,restar,multiplicar o dividir
+     * @param operacion tendra varias opciones, sumar,restar,multiplicar , dividir o raiz .
      * @param operacion de la division realizo un throw  Exception para capturar el error de la division entre 0.
      * @return un total.
      * @throws Exception
@@ -69,6 +74,10 @@ public class Metodos {
                 }
                 total = numero1 / numero2;
                 break;
+            case Raiz:
+                total = (float) Math.pow(numero1, (1/ numero2));
+                break;
+
 
         }
         return total;
@@ -79,13 +88,12 @@ public class Metodos {
      * @throws Exception
      */
     private static void validarOperacion(String operacion) throws Exception {
-        if (!operacion.equals("+") && !operacion.equals("-") && !operacion.equals("*") && !operacion.equals("/")) {
+        if (!operacion.equals("+") && !operacion.equals("-") && !operacion.equals("*") && !operacion.equals("/") && !operacion.equals("%")) {
             throw new Exception("La operacion inválida");
         }
 
     }
 }
-
 
 
 
