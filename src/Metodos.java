c66d79c536557d1f4f077c5f635f53f3665b1c3f
WindaLobo <wlobogarces@danielcastelao.org.com>
2023-02-24 09:45:39 +0100

    actualizacion

--- src/Metodos.java
+++ src/Metodos.java
@@ -16,9 +16,13 @@ public class Metodos {
      */
     private static final String Division = "/";
     /**
-     * Creo un atributo privado static final de Raiz  = "%".
+     * Creo un atributo privado static final de Raiz = "%".
      */
-    private static final String Raiz= "raiz";
+    private static final String Raiz = "%";
+    /**
+     * Creo un atributo privado static final de RaizCubico= "%".
+     */
+    private static final String RaizCubico = "%";
     /**
      * Creo un atributo privado static  de total en donde guardar los resultados .
      */
@@ -32,10 +36,10 @@ public class Metodos {
     public static void realizarOperacionBasicas() {
 
         try {
-            String operacion = LibreriaEntradSalida.entradaString("Que operacion quieres hacer " + "( \"+\" = Suma , \"-\" = Resta , \"*\" " + "= Multiplicacion , \"/\" = Division",LibreriaEntradSalida.CONSOLA);
+            String operacion = LibreriaEntradSalida.entradaString("Que operacion quieres hacer " + "( \"+\" = Suma , \"-\" = Resta , \"*\" " + "= Multiplicacion , \"/\" = Division  \"%\" = RaizCubico ", LibreriaEntradSalida.CONSOLA);
             validarOperacion(operacion);
-            float a = LibreriaEntradSalida.recibefloat("Introduce un numero",LibreriaEntradSalida.CONSOLA);
-            float b = LibreriaEntradSalida.recibefloat("Introduce un numero",LibreriaEntradSalida.CONSOLA);
+            float a = LibreriaEntradSalida.recibefloat("Introduce un numero", LibreriaEntradSalida.CONSOLA);
+            float b = LibreriaEntradSalida.recibefloat("Introduce un numero", LibreriaEntradSalida.CONSOLA);
             float resultado = operacionesBasicas(a, b, operacion);
             LibreriaEntradSalida.salida("El resulta es " + resultado, LibreriaEntradSalida.CONSOLA);
         } catch (Exception ex) {
@@ -47,7 +51,7 @@ public class Metodos {
     /**
      * Creo un método privado de operacionesBasicas que recibe tres parametros, float numero1, float numero2, String operacion
      *
-     * @param operacion tendra varias opciones, sumar,restar,multiplicar o dividir
+     * @param operacion tendra varias opciones, sumar,restar,multiplicar , dividir y raiz .
      * @param operacion de la division realizo un throw  Exception para capturar el error de la division entre 0.
      * @return un total.
      * @throws Exception
@@ -74,9 +78,8 @@ public class Metodos {
                 total = numero1 / numero2;
                 break;
             case Raiz:
-                total = (float)Math.pow(numero1,(1/numero2));
+                total = (float) Math.pow(numero1, (1 / numero2));
                 break;
-
 
 
         }
@@ -88,13 +91,12 @@ public class Metodos {
      * @throws Exception
      */
     private static void validarOperacion(String operacion) throws Exception {
-        if (!operacion.equals("+") && !operacion.equals("-") && !operacion.equals("*") && !operacion.equals("/")) {
+        if (!operacion.equals("+") && !operacion.equals("-") && !operacion.equals("*") && !operacion.equals("/") && !operacion.equals("%")) {
             throw new Exception("La operacion inválida");
         }
 
     }
 }
-
 
 
 
